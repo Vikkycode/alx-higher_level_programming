6b5f5d480ed9bd5800ac61e5434e77a46584681c
Victor <vicky.robilson5528@gmail.com>
2024-02-14 04:31:06 -0800

    7-insert_value

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- inserts a new row into the table first table
-INSERT INTO `first_table` (`id` INT, `name` VARCHAR) VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id` INT, `name` VARCHAR) 
+VALUES (89, "Holberton School");
