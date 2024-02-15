af19ab5ee0fc5725014730590ba905f383e75ab2
Victor <vicky.robilson5528@gmail.com>
2024-02-14 17:59:59 -0800

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
---list all records in the table second tble with a score >= 10
--- record are ordreed by descending score
+-- Lists all records in the table second_table with a score >= 10.
+-- Records are ordered by descending score.
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
+ORDER BY `score` DESC;
