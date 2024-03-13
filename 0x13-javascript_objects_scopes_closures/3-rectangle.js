eb4c336fcb086ed4abdbb1390cf34fc8d5341a17
Victor <vicky.robilson5528@gmail.com>
2024-03-12 17:36:33 -0700

    3 rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usb/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      const row = 'X';
+      console.log(row.repeat(this.width));
+    }
+  }
+};
