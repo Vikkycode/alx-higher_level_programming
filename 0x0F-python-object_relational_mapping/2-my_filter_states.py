bb53c5797ae81c9ec9d6eab3a8e6a830f5828429
Victor <vicky.robilson5528@gmail.com>
2024-03-21 21:17:03 -0700

    2 filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script to list all states from the database hbtn_0e_0_usa.
+
+This script connects to a MySQL server running on localhost at port 3306
+and retrieves all states from the database hbtn_0e_0_usa. The results are
+sorted in ascending order by states.id and displayed.
+
+Usage: ./list_states.py <username> <password> <database>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: MySQL database name
+
+Example:
+    ./list_states.py root root hbtn_0e_0_usa
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * \
+            FROM states WHERE \
+            BINARY name = '{}'".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
