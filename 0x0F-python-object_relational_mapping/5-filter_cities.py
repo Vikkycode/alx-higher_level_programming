f9c2c41a956ed795299ecdfcae7f4ecb63f21084
Victor <vicky.robilson5528@gmail.com>
2024-03-21 22:23:39 -0700

    5 filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -19,7 +19,7 @@ Example:
 import sys
 import MySQLdb
 
-if __name__=="__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM cities \
