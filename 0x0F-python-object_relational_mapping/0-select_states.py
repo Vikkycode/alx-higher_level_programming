8ac607b389e3ddfbf30750aa8852f4f960e57931
Victor <vicky.robilson5528@gmail.com>
2024-03-21 20:36:43 -0700

    0 state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
-# Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+"""
+Script to list all states from the database hbtn_0e_0_usa.
+
+This script connects to a MySQL server running on localhost at port 3306
+and retrieves all states from the database hbtn_0e_0_usa. The results are
+sorted in ascending order by states.id and displayed.
+
+Usage: ./list_states.py <username> <password> <database>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: MySQL database name
+
+Example:
+    ./list_states.py root root hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
