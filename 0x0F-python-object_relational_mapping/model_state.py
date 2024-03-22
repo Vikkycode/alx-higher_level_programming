c9d541dccc202bf5f401398216bc7d535386eb31
Victor <vicky.robilson5528@gmail.com>
2024-03-21 23:02:35 -0700

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Script to list all states from the database hbtn_0e_0_usa.
+
+This script connects to a MySQL server running on localhost at port 3306
+and retrieves all states from the database hbtn_0e_0_usa. The results are
+sorted in ascending order by states.id and displayed.
+
+Usage: ./list_states.py <username> <password> <database>
+
+Arguments:
+    username: MySQL username
+    password: MySQL password
+    database: MySQL database name
+
+Example:
+    ./list_states.py root root hbtn_0e_0_usa
+"""
+
 from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
