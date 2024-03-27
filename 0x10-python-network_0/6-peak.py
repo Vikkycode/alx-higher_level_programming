8177754facf05c86423d9bb24267266285bbf73a
Victor <vicky.robilson5528@gmail.com>
2024-03-27 15:54:07 -0700

    6 peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,24 @@
 
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
+    """
+    Return a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): A list of unsorted integers.
+
+    Returns:
+        int or None: A peak integer from the input list. Returns None if the
+        list is empty.
+
+    Example:
+        >>> find_peak([1, 2, 3, 4, 5])
+        5
+        >>> find_peak([5, 4, 3, 2, 1])
+        5
+        >>> find_peak([1, 3, 20, 4, 1, 0])
+        20
+    """
     if list_of_integers == []:
         return None
 
@@ -21,3 +38,8 @@ def find_peak(list_of_integers):
         return find_peak(list_of_integers[:mid])
     else:
         return find_peak(list_of_integers[mid + 1:])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
