f5928d8789e965e7185fdb06243fd513fc192410
Victor <vicky.robilson5528@gmail.com>
2024-03-28 16:12:53 -0700

    hbtn status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 
-with urllib.request.urlopen(request) as response:
+    with urllib.request.urlopen(request) as response:
     res = response.read()
     print("Body response:")
     print("\t - type: {}".format(type(res)))
