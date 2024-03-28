d80089dd54b0e04b766cca6f12c7580f8aafd240
Victor <vicky.robilson5528@gmail.com>
2024-03-28 16:46:57 -0700

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
 with urllib.request.urlopen(req) as response:
     res = response.read()
     print("Body response:")
-    print("\t - type: {}".format(type(res)))
-    print("\t - content: {}".format(res))
-    print("\t - utf8 content: {}".format(res.decode('utf8')))
+    print("\t- type: {}".format(type(res)))
+    print("\t- content: {}".format(res))
+    print("\t- utf8 content: {}".format(res.decode('utf8')))
